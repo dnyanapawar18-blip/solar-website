6ede280d6b771e646da955901dc5dbbed7abd99d
Dnyaneshwari <dnyana.pawar18@gmail.com>
2026-05-27 22:41:49 -0700

    Final working solar website

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 ========================= */
 app.use(cors({
     origin: "*",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST","OPTIONS"],
     allowedHeaders: ["Content-Type"]
 }));
 
